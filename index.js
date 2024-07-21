e7caace30af60e63ab87ed3dc5740e9cb248ad79
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2024-07-22 01:05:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+//import cors from "cors";
 import { connectDB } from "./config/dbConnection.js";
 import { songsRouter } from "./routes/songRoutes.js";
 import { userRouter } from "./routes/userRoutes.js";
@@ -12,14 +12,14 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 //app.use(cors(corsOptions));
-app.use(cors());
-// app.use((req, res, next) =>{
-// 	//	  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000/');
-// 		  res.setHeader('Access-Control-Allow-Origin','https://musicfrontend2.vercel.app/');
-// 		  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT,PATCH, DELETE');
-// 		  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// 		  next();
-// 		});
+//app.use(cors());
+app.use((req, res, next) =>{
+		  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000/');
+	//	  res.setHeader('Access-Control-Allow-Origin','https://musicfrontend2.vercel.app/');
+		  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT,PATCH, DELETE');
+		  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+		  next();
+		});
 
 connectDB();
 
